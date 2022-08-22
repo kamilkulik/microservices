216a9935231e18b780495493582e96c67195ed39
Kamil Kulik <kulikkamil@gmail.com>
2022-08-22 10:14:11 +0200

    [TOY_APP] handle comment status in React

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -2,7 +2,17 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === 'approved') {
+      content = comment.content
+    }
+    if (comment.status === 'pending') {
+      content = 'pending moderation'
+    }
+    if (comment.status === 'rejected') {
+      content = 'comment rejected'
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
