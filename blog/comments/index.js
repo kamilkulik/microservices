3d38edd18c87882e6b6d19096b5664891af1fdd9
Kamil Kulik <kulikkamil@gmail.com>
2022-08-22 10:14:10 +0200

    [TOY_APP] event-bus init

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,10 +1,13 @@
+const axios = require('axios');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 
 const { randomBytes } = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -12,15 +15,33 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  }).catch((err) => {
+    console.error(err.message);
+  })
+
   res.status(201).send(comments);
 });
+
+app.post('/events', (req, res) => {
+  console.log('Received event: ', req.body.type)
+
+  res.send({});
+})
 
 app.listen(4001, () => {
   console.log("Listening on port 4001");
---
+++ blog/event-bus/index.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+  
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: 'OK' });
+})
+
+app.listen(4005, () => {
+  console.log("Listening on port 4005");
+})
