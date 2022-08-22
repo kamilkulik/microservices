b92a12e539886ecc445e7932aa5367bcdd794481
Kamil Kulik <kulikkamil@gmail.com>
2022-08-22 10:14:11 +0200

    [TOY_APP] handle lost events

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,8 +5,16 @@ const express = require('express');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
+app.get('/events', (req, res) => {
+  res.send(events);
+})
+
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
   
   axios.post('http://localhost:4000/events', event).catch((err) => {
     console.log(err.message);
--- blog/query/index.js
+++ blog/query/index.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const express = require('express');
@@ -9,23 +10,16 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-})
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-  const { id, content, postId, status } = data;
-
+const handleEvent = (type, data) => {
+  const { id, content, postId, status, title } = data;
   if (type === 'PostCreated') {
-    const { id, title } = data;
 
     posts[id] = { id, title, comments: []};
   }
 
   if (type === 'CommentCreated') {
     const post = posts[postId];
-    post.comments.push({ id, content, status })
+    post.comments.push({ id, content, status });
   }
 
   if (type === 'CommentUpdated') {
@@ -36,13 +30,33 @@ app.post('/events', (req, res) => {
         comment.content = content;
       }
     })
-    
   }
+}
 
-  console.log(posts);
+app.get('/posts', (req, res) => {
+  res.send(posts);
+})
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+
+  handleEvent(type, data);
   res.send({});
 })
 
-app.listen(4002, () => {
-  console.log('Query serv listening on 4002')
+app.listen(4002, async () => {
+  console.log('Query serv listening on 4002');
+
+  try {
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+      console.log('Processing event: ', event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch(err) {
+    console.error(err.message);
+  }
 })
