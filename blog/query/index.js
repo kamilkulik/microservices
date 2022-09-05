7d82c7e3212f1b001db7db82f65cb56362b5596c
Kamil Kulik <kulikkamil@gmail.com>
2022-09-05 18:19:57 +0200

    [TOY_APP] update comments, moderation, query services to use Cluster IP service names for networking

--- blog/query/index.js
+++ blog/query/index.js
@@ -49,7 +49,7 @@ app.listen(4002, async () => {
   console.log('Query serv listening on 4002');
 
   try {
-    const res = await axios.get('http://localhost:4005/events');
+    const res = await axios.get('http://event-bus-srv:4005/events');
 
     for (let event of res.data) {
       console.log('Processing event: ', event.type);
